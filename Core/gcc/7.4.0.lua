3bc8bb799e703ec53b5966f170ecc9bc84a2cb19
Vladimir <vladimir.dikan@icn2.cat>
2019-01-17 14:13:57 +0100

    modules metadata

--- Core/gcc/7.4.0.lua
+++ Core/gcc/7.4.0.lua
@@ -1,15 +1,30 @@
 -- GCC-7.4.0
 -- Setup Modulepath for packages built by this compiler
+help([[
+GCC compiler module file.
+]])
+
+local version = "7.4"
+
+whatis("Name: GCC compiler")
+whatis("Version: " .. version)
+whatis("Keywords: System, Compiler")
+whatis("URL: http://www.gnu.org/")
+whatis("Description: GNU compiler family")
+
 family("compiler")
-local mroot = os.getenv("MODULEPATH_ROOT")
-local mdir  = pathJoin(mroot,"Compiler/gcc", "7.4")
-prepend_path("MODULEPATH", mdir)
 -- Actual path setter.
 -- Makes `gcc-7.4`, `gfortran-7.4`, `g++-7.4` etc visible on this machine.
 -- Unlike binaries with major version only in the title, these were compiled
 -- locally and not installed through package manager.
-prepend_path("PATH", "/usr/local/gcc-7.4/bin")
+local prefix = "/usr/local/gcc-7.4/bin"
+
+prepend_path("PATH", prefix)
 -- Set enviroment variables for Makefile macro.
-pushenv("FC", "/usr/local/gcc-7.4/bin/gfortran-7.4")
-pushenv("CC", "/usr/local/gcc-7.4/bin/gcc-7.4")
-pushenv("CXX", "/usr/local/gcc-7.4/bin/g++-7.4")
+pushenv("FC",  pathJoin(prefix, "gfortran-7.4"))
+pushenv("CC",  pathJoin(prefix, "gcc-7.4"))
+pushenv("CXX", pathJoin(prefix, "g++-7.4"))
+
+local mroot = os.getenv("MODULEPATH_ROOT")
+local mdir  = pathJoin(mroot,"Compiler/gcc", "7.4")
+prepend_path("MODULEPATH", mdir)
--- Core/gcc/8.2.0.lua
+++ Core/gcc/8.2.0.lua
@@ -1,15 +1,30 @@
 -- GCC-8.2.0
 -- Setup Modulepath for packages built by this compiler
+help([[
+GCC compiler module file.
+]])
+
+local version = "8.2"
+
+whatis("Name: GCC compiler")
+whatis("Version: " .. version)
+whatis("Keywords: System, Compiler")
+whatis("URL: http://www.gnu.org/")
+whatis("Description: GNU compiler family")
+
 family("compiler")
-local mroot = os.getenv("MODULEPATH_ROOT")
-local mdir  = pathJoin(mroot,"Compiler/gcc", "8.2")
-prepend_path("MODULEPATH", mdir)
 -- Actual path setter.
 -- Makes `gcc-8.2`, `gfortran-8.2`, `g++-8.2` etc visible on this machine.
 -- Unlike binaries with major version only in the title, these were compiled
 -- locally and not installed through package manager.
-prepend_path("PATH", "/usr/local/gcc-8.2/bin")
+local prefix = "/usr/local/gcc-8.2/bin"
+
+prepend_path("PATH", prefix)
 -- Set enviroment variables for Makefile macro.
-pushenv("FC", "/usr/local/gcc-8.2/bin/gfortran-8.2")
-pushenv("CC", "/usr/local/gcc-8.2/bin/gcc-8.2")
-pushenv("CXX", "/usr/local/gcc-8.2/bin/g++-8.2")
+pushenv("FC",  pathJoin(prefix, "gfortran-8.2"))
+pushenv("CC",  pathJoin(prefix, "gcc-8.2"))
+pushenv("CXX", pathJoin(prefix, "g++-8.2"))
+
+local mroot = os.getenv("MODULEPATH_ROOT")
+local mdir  = pathJoin(mroot,"Compiler/gcc", "8.2")
+prepend_path("MODULEPATH", mdir)
