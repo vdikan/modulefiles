40a58f79b03ff1785baa1d06ee35116cef31e2fc
Vladimir <vladimir.dikan@icn2.cat>
2019-01-18 15:59:14 +0100

    default scalapack

---
+++ MPI/gcc/8.2/openmpi/4.0/scalapack/2.0.2.lua
@@ -0,0 +1,5 @@
+whatis("Name: SCALAPACK")
+whatis("Version: 2.0.2")
+whatis("Only the SCALAPACK_LIBS variable is set")
+
+setenv("SCALAPACK_LIBS", "-L/usr/local/apps-gcc-8.2/apps-openmpi-4.0.0/scalapack/2.0.2/lib -lscalapack")
